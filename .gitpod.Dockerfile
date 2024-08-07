1729e0b89a1bd6c11620313d1471d1fdf66368f1
Pavel <pavel@testlio.com>
2024-08-07 19:25:25 +0300

    Refactored the project for Gitpod

--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -1,14 +1,17 @@
-FROM gitpod/workspace-full-vnc:latest
+FROM gitpod/workspace-full-vnc
 
-# Install dependencies
-#RUN sudo apt-get install -y \
-#    libgtk2.0-0 \
-#    libgtk-3-0 \
-#    libnotify-dev \
-#    libnss3 \
-#    libxss1 \
-#    libasound2 \
-#    libxtst6 \
-#    xauth \
-#    xvfb \
-#    && sudo rm -rf /var/lib/apt/lists/*
+ENV CYPRESS_CACHE_FOLDER=/workspace/.cypress-cache
+
+# Install Cypress dependencies
+RUN sudo apt-get update \
+    && sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
+    libgtk2.0-0 \
+    libgtk-3-0 \
+    libnotify-dev \
+    libnss3 \
+    libxss1 \
+    libasound2 \
+    libxtst6 \
+    xauth \
+    xvfb \
+    && sudo rm -rf /var/lib/apt/lists/*
--- wdio.conf.js
+++ wdio.conf.js
@@ -1,13 +1,10 @@
-export const config = {
+exports.config = {
     //
     // ====================
     // Runner Configuration
     // ====================
     // WebdriverIO supports running e2e tests as well as unit and component tests.
     runner: 'local',
-    // hostname: '0.0.0.0', // This allows connections from Gitpod
-    port: 4444, // ChromeDriver port
-    // path: '/',
     //
     // ==================
     // Specify Test Files
@@ -46,18 +43,14 @@ export const config = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
-    maxInstances: 1,
+    maxInstances: 10,
     //
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome',
-        acceptInsecureCerts: true,
-        'goog:chromeOptions': {
-            args: ['headless', 'disable-gpu']
-        }
+        browserName: 'chrome'
     }],
 
     //
@@ -107,7 +100,7 @@ export const config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-
+    // services: [],
     //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
